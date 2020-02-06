4115310e3433c30c45ee9cabf8633dff2a697a47
Saurabha <jirgi.s@husky.neu.edu>
2020-02-05 19:11:13 -0500

    Added story CRUD operations

--- routes/story.route.server.js
+++ routes/story.route.server.js
@@ -12,7 +12,20 @@ module.exports = app => {
             res.send(JSON.stringify(stories, null, '\t'))
     });
 
+    createStory = (req, res) =>
+        storyDao.createStory(req.body).then(story => res.json(story))
+
+    deleteStory = (req, res) =>
+        storyDao.deleteStory(req.params.storyId).then(story => res.json(story))
+
+    updateStory = (req, res) =>
+        storyDao.updateStory(req.params.storyId, req.body).then(story => res.json(story))
+
+
     app.get('/stories', findAllStories);
     app.get('/stories/story/:storyID', findStoryByStoryID);
     app.get('/stories/place/:placeID',findStoryByPlaceID);
+    app.post('/stories/create', createStory)
+    app.delete('/stories/:storyId', deleteStory)
+    app.put('/stories/:storyId', updateStory)
 };
