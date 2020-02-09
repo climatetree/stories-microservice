2e47f1703fd38a6748ba2a6a5fcb14d885595231
spica <spicaofsky@gmail.com>
2020-02-08 16:57:15 -0800

    Did some update on conf.

--- db/db.js
+++ db/db.js
@@ -3,14 +3,13 @@
  */
 
 module.exports = function () {
-    const mongoose = require('mongoose'),Admin=mongoose.mongo.Admin;
-    let mongoPORT = process.env.MONGO_PORT|| '10255';
-    let accountName='stories-mongodb';
-    let databaseName='climateTree';
-    let key='KKYmGrO8a2NUL6jGaawC3BPd0eM5YxRA8gVucNsCAcTikrfl0BQVaY1bqNcaslvYoHJQU9QmYqzcuw2c03hypQ==';
+    const mongoose = require('mongoose');
+    const mongoPORT = '10255';
+    const accountName='stories-mongodb';
+    const databaseName='climateTree';
+    const key='KKYmGrO8a2NUL6jGaawC3BPd0eM5YxRA8gVucNsCAcTikrfl0BQVaY1bqNcaslvYoHJQU9QmYqzcuw2c03hypQ==';
     const mongoUri = `mongodb://${accountName}:${key}@${accountName}.documents.azure.com:${mongoPORT}/${databaseName}?ssl=true`;
-    let mongoDB = process.env.MONGODB_URI || mongoUri;
     mongoose.set('debug', true);
-    mongoose.connect(mongoDB,{useNewUrlParser: true});
+    mongoose.connect(mongoUri,{useNewUrlParser: true});
   };
   
