867a9a335e8377b427e24765801c52161393dc01
patelvp <patel.visha@husky.neu.edu>
2020-02-09 22:18:45 -0500

    Fix docker push script

--- docker_push.sh
+++ docker_push.sh
@@ -2,4 +2,4 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t climatetree:backend-mongo .
 docker tag climatetree:backend-mongo patelvp/climatetree:backend-mongo
-docker push patelvp/climatetree
+docker push patelvp/climatetree:backend-mongo
