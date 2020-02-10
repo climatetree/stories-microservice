f538d155968eeb4e9ec7277e01ac87957a51cda5
patelvp <patel.visha@husky.neu.edu>
2020-02-10 09:53:26 -0500

    Use env variables in connection arguments

--- db/db.js
+++ db/db.js
@@ -3,12 +3,12 @@
  */
 
 module.exports = function () {
-    if(process.env.CI){
+    if(process.env.DOCKER_ENABLE_CI){
         const mongoose = require('mongoose'),Admin=mongoose.mongo.Admin;
         let mongoPORT = process.env.MONGO_PORT|| '10255';
-        let accountName='stories-mongodb';
+        let accountName=process.env.MONGO_ACCOUNT_NAME;
         let databaseName='climateTree';
-        let key='KKYmGrO8a2NUL6jGaawC3BPd0eM5YxRA8gVucNsCAcTikrfl0BQVaY1bqNcaslvYoHJQU9QmYqzcuw2c03hypQ==';
+        let key=process.env.MONGO_KEY;
         const mongoUri = `mongodb://${accountName}:${key}@${accountName}.documents.azure.com:${mongoPORT}/${databaseName}?ssl=true`;
         let mongoDB = process.env.MONGODB_URI || mongoUri;
         mongoose.set('debug', true);
