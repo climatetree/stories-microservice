b889975a940d8cb6ee7e6815d36b1f2ea24f7301
spica <spicaofsky@gmail.com>
2020-02-16 15:23:50 -0800

    Use keyword to search in title added.

--- routes/story.route.server.js
+++ routes/story.route.server.js
@@ -2,22 +2,23 @@ storyDao = require('../dao/story.dao.server');
 module.exports = app => {
 
     findAllStories = (req, res) => 
-        storyDao.findAllStories().then(stories => {
-            console.log(stories);
-            res.json(stories);
-        });
+        storyDao.findAllStories().then(stories => res.json(stories));
 
     findStoryByStoryID = (req, res) =>
-        storyDao.findStoryByStoryID(req.params.storyID).then(story => {
-            console.log(story);
-            res.json(story);
-        });
+        storyDao.findStoryByStoryID(req.params.storyID).then(story => res.json(story));
 
     findStoryByPlaceID = (req,res)=>
         storyDao.findStoryByPlaceID(req.params.placeID).exec(function (err,stories) {
             console.log(stories);
             res.send(JSON.stringify(stories, null, '\t'))
     });
+
+
+    findStoryByTitle = (req,res)=>
+        storyDao.findStoryByTitle(req.params.title).exec(function (err,stories) {
+            console.log(req.params.title);
+            res.send(JSON.stringify(stories, null, '\t'))
+        });
 
     createStory = (req, res) =>
         storyDao.createStory(req.body).then(story => res.json(story));
@@ -32,6 +33,7 @@ module.exports = app => {
     app.get('/stories', findAllStories);
     app.get('/stories/story/:storyID', findStoryByStoryID);
     app.get('/stories/place/:placeID',findStoryByPlaceID);
+    app.get('/stories/title/:title',findStoryByTitle)
     app.post('/stories/create', createStory);
     app.delete('/stories/delete/:storyId', deleteStory);
     app.put('/stories/update/:storyId', updateStory);
