0f9c611ce1604cb7e329d0cb47aab3ae9e5253d6
Kanika <krana@ccs.neu.edu>
2020-03-15 20:29:54 -0400

    fixed code smells

--- dao/comment.dao.server.js
+++ dao/comment.dao.server.js
@@ -1,16 +1,16 @@
 const commentModel = require('../models/comments.model.server')
 var ObjectID = require("bson-objectid");
 
-findAllComments = () => commentModel.find();
+let findAllComments = () => commentModel.find();
 
-findCommentById = (commentId) => commentModel.findOne({comment_id:commentId});
+let findCommentById = (commentId) => commentModel.findOne({comment_id:commentId});
 
-deleteComment = (commentId) => commentModel.deleteOne({comment_id:commentId},function(err, obj) {
+let deleteComment = (commentId) => commentModel.deleteOne({comment_id:commentId},function(err, obj) {
     if (err) throw err;
     console.log("1 document deleted");
 });
 
-addComment = (userId, comment, date) => {
+let addComment = (userId, comment, date) => {
     const commentId = ObjectID().str;
     return commentModel.create({
         comment_id: commentId,
