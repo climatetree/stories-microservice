62110004347ccbf3aabc7d04149028f1533f20e7
Siddhant Varyambat <vpsiddhant@gmail.com>
2020-03-26 03:02:29 -0700

    Add roles Modify endpoint to delete comment by admin and mods Modify test to add roles

---
+++ constants/role.js
@@ -0,0 +1,12 @@
+ADMIN = "admin";
+MODERATOR = "mod";
+REGISTERED_USER ="reg_user";
+GUEST_USER = "guest_user";
+
+
+module.exports = {
+    ADMIN,
+    MODERATOR,
+    REGISTERED_USER,
+    GUEST_USER
+};
