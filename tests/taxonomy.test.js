138ea2fcc88d72bdea04ba40ca737e2ff8b43537
Vishal Patel <38330754+patelvp@users.noreply.github.com>
2020-04-10 17:20:10 -0400

    Release (#100)
    * added api to return flagged stories in descending order of number of flags
    
    * Change query for sorting according to length of array instead of sum of array elements
    Add check for invalid parameters
    Add test for invalid parameters
    
    * Taxonomy schema added.
    
    * New API for taxonomy added:
    + findAllTaxonomy
    + findTaxonomyBySolution
    + findTaxonomyBySector
    + findTaxonomyByStrategy
    
    * New API for taxonomy endpoints added:
    + app.get('/stories/taxonomy',findAllTaxonomy);
    + app.get('/stories/taxonomy/solution/:solution',findTaxonomyBySolution);
    + app.get('/stories/taxonomy/strategy/:strategy',findTaxonomyByStrategy);
    + app.get('/stories/taxonomy/sector/:sector',findTaxonomyBySector);
    
    * Tests for API in taxonomy.dao and endpoints added.
    
    * Update port in taxonomy.test.js
    
    Update the port used in taxonomy.test.js
    
    * Versioned APIs for stories, comments and taxonomy APIs to version 1
    
    * Updated tests to use the version 1 APIs
    
    * Removed duplicate/unused code
    
    * fixed breaking tests after merging
    
    Co-authored-by: Kanika <rana.kanika.2009@gmail.com>
    Co-authored-by: Siddhant Varyambat <vpsiddhant@gmail.com>
    Co-authored-by: spica <spicaofsky@gmail.com>
    Co-authored-by: nehagundecha <gundecha.n@husky.neu.edu>
    Co-authored-by: Saurabha <jirgi.s@husky.neu.edu>
    Co-authored-by: SaurabhaJirgi <46516350+SaurabhaJirgi@users.noreply.github.com>
    Co-authored-by: Neha Gundecha <38900196+nehagundecha95@users.noreply.github.com>
    Co-authored-by: ranakan19 <46766610+ranakan19@users.noreply.github.com>

---
+++ tests/taxonomy.test.js
@@ -0,0 +1,79 @@
+const request = require('supertest');
+
+const dbHandler = require('./db.handler');
+const taxonomyDao = require('../dao/taxonomy.dao.server');
+const taxonomyModel=require('../models/taxonomy.model.server');
+
+const app = require('../app');
+let server, agent;
+
+
+describe('End Points for Stories', () => {
+    /**
+     * connect to the in-memory database before running the tests
+     */
+    const t1=new taxonomyModel({strategy:"tmp1",solution:"tmp1",sector:"tmp1"});
+    const t2=new taxonomyModel({strategy:"tmp2",solution:"tmp2",sector:"tmp2"});
+    const t3=new taxonomyModel({strategy:"tmp3",solution:"tmp3",sector:"tmp3"});
+
+    beforeAll(async (done) => {
+        await dbHandler.connect();
+        server = app.listen(3002, (err) => {
+            if (err) return done(err);
+            agent = request.agent(server); // since the application is already listening, it should use the allocated port
+            t1.save();t2.save();t3.save();
+            done();
+        });
+    });
+
+    /**
+     * after all the tests clear the database and close the database connection
+     */
+    afterAll(async (done) => {
+        await dbHandler.clearDatabase();
+        await dbHandler.closeDatabase();
+        return server && server.close(done);
+    });
+
+    it('can return all the taxonomy in the database', async () => {
+        const result = await taxonomyDao.findAllTaxonomy();
+        expect(result.length).toEqual(3);
+    });
+
+    it('can return the taxonomy by solution in the database', async () => {
+        const result = await taxonomyDao.findTaxonomyBySolution("tmp1");
+        expect(result.length).toEqual(1);
+    });
+
+    it('can return the taxonomy by sector in the database', async () => {
+        const result = await taxonomyDao.findTaxonomyBySolution("tmp1");
+        expect(result.length).toEqual(1);
+    });
+
+    it('can return the taxonomy by strategy in the database', async () => {
+        const result = await taxonomyDao.findTaxonomyBySolution("tmp1");
+        expect(result.length).toEqual(1);
+    });
+
+    describe('GET/',()=>{
+        it('/v1/stories/taxonomy can return the taxonomy by solution in the database',  (done) => {
+            request(app).get('/v1/stories/taxonomy').set('Accept', 'application/json')
+                .expect(200, done);
+        });
+
+        it('/v1/stories/taxonomy/solution/:solution can return the taxonomy by solution in the database',  (done) => {
+            request(app).get('/v1/stories/taxonomy/solution/tmp1').set('Accept', 'application/json')
+                .expect(200, done);
+        });
+
+        it('/v1/stories/taxonomy/strategy/:strategy can return the taxonomy by strategy in the database',  (done) => {
+            request(app).get('/v1/stories/taxonomy/strategy/tmp1').set('Accept', 'application/json')
+                .expect(200, done);
+        });
+
+        it('/v1/stories/taxonomy/sector/:sector can return the taxonomy by sector in the database',  (done) => {
+            request(app).get('/v1/stories/taxonomy/sector/tmp1').set('Accept', 'application/json')
+                .expect(200, done);
+        });
+    })
+});
