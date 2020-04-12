c126be60ca10c990056bcf997362fc2d536addd8
spica <spicaofsky@gmail.com>
2020-04-12 13:25:19 -0700

    Add following feature.
    Try to connect to ES when app starts. If failed then use pure mongodb instead.
    Otherwise created all current stories indexes on elasticsearch.

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');// initialize our express app
 const app = express();
+const es=require('./db/elasticsearch');
 
 app.use((req, res, next) => {
-  const allowedOrigins = ['http://localhost:3000', ' https://climatetree.azurewebsites.net'];
-  const origin = req.headers.origin;
-  if(allowedOrigins.indexOf(origin) > -1){
-      res.setHeader('Access-Control-Allow-Origin', origin);
-  }
+    const allowedOrigins = ['http://localhost:3000', ' https://climatetree.azurewebsites.net'];
+    const origin = req.headers.origin;
+    if(allowedOrigins.indexOf(origin) > -1){
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
 
-  res.header("Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
+    res.header("Access-Control-Allow-Headers",
+               "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+               "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
 });
 
 app.use(bodyParser.json());
@@ -28,22 +29,27 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json({
-             message: err.message,
-             error: err
-           });
+    // render the error page
+    res.status(err.status || 500);
+    res.json({
+                 message: err.message,
+                 error: err
+             });
 });
 
-require('./db/db')()
+require('./db/db')();
+es.connectES();
+let storyService;
+if(es.esClient===null){
+    console.log("cannot connect to elasticsearch, using only mongodb instead.");
+    storyService=require('./routes/story.route.server');
+}else{
+    storyService =require('./routes/es.story.route.server');
+}
 
-const storyService = require('./routes/story.route.server')
-storyService(app)
-
-
+storyService(app);
 module.exports = app;
